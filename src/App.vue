1867a077c943bc90a0f112dd5b69bddcbf181af0
DoniyorbekErkinov <erkinovdoniyor@gmail.com>
2024-10-12 12:44:26 +0500

    default routes done

--- src/App.vue
+++ src/App.vue
@@ -1,79 +1,11 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
-  <header>
-    <nav>
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/about">About</RouterLink>
-    </nav>
-  </header>
-
-  <RouterView />
+  <div>
+    <RouterView />
+  </div>
 </template>
 
-<style scoped>
-header {
-  max-height: 100vh;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-  display: flex;
-  justify-content: center;
-}
-
-nav a.router-link-exact-active {
-  color: green;
-  font-weight: 600;
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  color: green;
-  font-weight: 400;
-  letter-spacing: 2px;
-  text-decoration: none;
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+<style scoped></style>
---
+++ src/Layouts/Default.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { useRoute } from "vue-router";
+import Header from "./Header.vue";
+
+const route = useRoute();
+</script>
+<template>
+  <div>
+    <Header />
+    <main class="main" id="main">
+      <router-view v-slot="{ Component }">
+        <Transition name="fade" made="out-in">
+          <component :is="Component"></component>
+        </Transition>
+      </router-view>
+    </main>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
